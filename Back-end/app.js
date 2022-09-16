c497c01a4d3a35202f6f07304d36c4253c23170a
GyovanneMS <Gyovannecontato@gmail.com>
2022-09-16 11:24:47 -0300

    Add all

---
+++ Back-end/app.js
@@ -0,0 +1,80 @@
+/*=========================================================
+* Objetivo: API para o trabalho com alunos;
+* Data: 15/09/22
+* Autor: Gyovanne Martins
+* Versão: 1.0.0
+=========================================================*/
+
+//import da biblioteca da express para criar a API
+const express = require("express");
+
+//import da biblioteca do cors para manipular as permissões de protocolo HTTP
+const cors = require("cors");
+
+//import da bibliotexa do body-parser que irá manipular o corpo das requisições do protocolo HTTP
+const bodyParser = require("body-parser");
+
+const { iconeSigla } = require("./modulos/infoCurso.js");
+const { infoAlunos, infoUmAluno } = require("./modulos/infoAlunos.js");
+
+const app = express();
+
+app.use((request, response, next) => {
+    //Permite especificar quem serão os IPs que podem acessar a API (* -> ssignifica todos)
+    response.header('Access-Control-Allow-Origin', '*');
+
+    //Permite especificar quais serão os verbos ou métodos que a API irá reconhecer
+    response.header('Access-Control-Allow-Methodos', 'GET, POST, PUT, DELETE, OPTIONS');
+
+    //Estabelece que as permissões acima serão representadas pelo cors
+    app.use(cors());
+
+    next();
+});
+
+app.get('/informacoesDosCursos', cors(), async function(request, response, next){
+    //recebe a sigla enviada por parametro no endpoint
+    let cursos = iconeSigla();
+
+    if(cursos){
+        response.status(200);
+        response.json(cursos);
+    } else{
+        response.status(404);
+    }
+
+})//Fazer o teste no Postman
+
+app.get('/alunos/:curso', cors(), async function(request, response, next){
+    //recebe a sigla enviada por parametro no endpoint
+    let curso = request.params.curso;
+    //let curso = 'DS'
+    let alunos = infoAlunos(curso);
+
+    if(alunos){
+        response.status(200);
+        response.json(alunos);
+    } else{
+        response.status(404);
+    }
+})
+
+app.get('/disciplinas/:aluno', cors(), async function(request, response, next){
+    //recebe a sigla enviada por parametro no endpoint
+    let aluno = request.params.aluno;
+    //let curso = 'DS'
+    let disciplinas = infoUmAluno(aluno);
+
+    if(disciplinas){
+        response.status(200);
+        response.json(disciplinas);
+    } else{
+        response.status(404);
+    }
+})
+
+
+//Para que os EndPoints possam estar funcionando, precisamos obrigatoriamente finalizar a API nessa  function, que representa o start da API
+app.listen(8080, function(){
+    console.log('Servidor aguardando requisições...')
+});
---
+++ Back-end/modulos/infoCurso.js
@@ -0,0 +1,40 @@
+/*=========================================================
+* Objetivo: Obter uma lista com as informações mais apropriadas de um curso
+* Data: 09/15/22
+* Autor: Gyovanne Martins
+* Versão: 1.0.0
+*/
+
+
+var cursos = [
+    {
+        "nome"  :   "001 - Técnico em Desenvolvimento de Sistemas",
+        "sigla" :   "DS",
+        "icone" :   "https://image.shutterstock.com/image-vector/api-interface-vector-icon-600w-659203513.jpg",
+        "carga" :   "1200",
+    },
+    {
+        "nome"  :   "002 - Técnico em Redes de Computadores",
+        "sigla" :   "RDS",
+        "icone" :   "https://img.icons8.com/ultraviolet/344/thin-client.png",
+        "carga" :   "1200"
+    }
+];
+
+
+
+const iconeSigla = function(){
+    let infoArray = [];
+    let infoJson = {};
+        //Percorre o array até achar a sigla ou o nome da matéria
+        cursos.forEach(ListaCurso => {
+            infoArray.push({Icone : ListaCurso.icone, Sigla : ListaCurso.sigla});
+            infoJson.curso = infoArray;
+        })
+    return infoJson;
+}
+
+
+module.exports = {
+    iconeSigla
+}
